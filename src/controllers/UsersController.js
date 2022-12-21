d0fbe0e157b863aaeb9d59a14d6aa4cb27bc1aa1
ymillerr <millerandvitor@gmail.com>
2022-12-21 09:18:42 +0100

    finished backend

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
-const { hash } = require("bcryptjs")
-const AppError = require('../utils/AppError')
+const { hash , compare} = require("bcryptjs");
+const AppError = require('../utils/AppError');
 const sqliteConnetion = require('../database/sqlite');
 
 class UsersController {
@@ -18,7 +18,54 @@ class UsersController {
         await database.run("INSERT INTO users (name , email , password) VALUES (? , ? , ?)" , [name , email , hashedPassword]);
 
         return response.status(201).json();
-    };
-};
+    }
+
+    async update(request , response){
+        const { name , email , password , old_password } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnetion();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)" , [id]);
+
+        if(!user){
+            throw new AppError("Usuário não encontrado");
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)" , [email])
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+            throw new AppError("Esta E-mail já está em uso!")
+        }
+
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        if(password && !old_password){
+            throw new AppError("Você precisa informar a senha antiga")
+        }
+
+        if(password && old_password){
+            const checkOldPassword = await compare(old_password , user.password)
+
+            if(!checkOldPassword){
+                throw new AppError("Senha antiga incorreta")
+            }
+
+            user.password = await hash(password , 8)
+        }
+
+        await database.run(`
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        password = ?,
+        updated_at = DATETIME('now')
+        WHERE id = ?`,
+        [user.name , user.email , user.password , id])
+
+        return response.json();
+
+    }
+}
 
 module.exports = UsersController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
-const { Router } = require("express")
+const { Router } = require("express");
 
-const userRoutes = require("./users.routes")
+const userRoutes = require("./users.routes");
+const notesRoutes = require("./notes.routes");
+const tagsRouter = require("./tags.routes")
 
-const routes = Router()
+const routes = Router();
 
-routes.use("/users" , userRoutes)
+routes.use("/users" , userRoutes);
+routes.use("/notes" , notesRoutes);
+routes.use("/tags" , tagsRouter);
 
-module.exports = routes
+module.exports = routes;
